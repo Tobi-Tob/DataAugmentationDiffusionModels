59532c91646df1b23cc8b59a4ff50285a824dda7
Tobi-Tob <tobi.lingenberg@t-online.de>
2023-12-06 14:19:36 +0100

    Changing architecture of ClassificationFilterModel

--- train_filter.py
+++ train_filter.py
@@ -77,8 +77,7 @@ def train_filter(seed: int = 0,
         sampler=val_sampler, num_workers=4)
 
     filter_model = ClassificationFilterModel(
-        train_dataset.num_classes,
-        backbone="resnet50"
+        train_dataset.num_classes
     ).cuda()
 
     optim = torch.optim.Adam(filter_model.parameters(), lr=0.0001)
@@ -229,49 +228,29 @@ def train_filter(seed: int = 0,
 
 class ClassificationFilterModel(nn.Module):
 
-    def __init__(self, num_classes: int, backbone: str = "resnet50"):
+    def __init__(self, num_classes: int):
 
         super(ClassificationFilterModel, self).__init__()
 
-        self.backbone = backbone
         self.image_processor = None
-
-        if backbone == "resnet50":
-
-            self.base_model = resnet50(weights=ResNet50_Weights.DEFAULT)
-            self.out = nn.Linear(2048, num_classes)
-
-        elif backbone == "deit":
-
-            self.base_model = DeiTModel.from_pretrained(
-                "facebook/deit-base-distilled-patch16-224")
-            self.out = nn.Linear(768, num_classes)
+        self.base_model = resnet50(weights=ResNet50_Weights.DEFAULT)
+        self.out = nn.Linear(2048, num_classes)
 
     def forward(self, image):
-
         x = image
+        with torch.no_grad():
 
-        if self.backbone == "resnet50":
+            x = self.base_model.conv1(x)
+            x = self.base_model.bn1(x)
+            x = self.base_model.relu(x)
+            x = self.base_model.maxpool(x)
 
-            with torch.no_grad():
+            x = self.base_model.layer1(x)
+            x = self.base_model.layer2(x)
+            x = self.base_model.layer3(x)
+            x = self.base_model.layer4(x)
 
-                x = self.base_model.conv1(x)
-                x = self.base_model.bn1(x)
-                x = self.base_model.relu(x)
-                x = self.base_model.maxpool(x)
-
-                x = self.base_model.layer1(x)
-                x = self.base_model.layer2(x)
-                x = self.base_model.layer3(x)
-                x = self.base_model.layer4(x)
-
-                x = self.base_model.avgpool(x)
-                x = torch.flatten(x, 1)
-
-        elif self.backbone == "deit":
-
-            with torch.no_grad():
-
-                x = self.base_model(x)[0][:, 0, :]
+            x = self.base_model.avgpool(x)
+            x = torch.flatten(x, 1)
 
         return self.out(x)
