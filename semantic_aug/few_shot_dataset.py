f213ae6f154d3e19157e85d7969f968ba7a460a7
KingEsel1 <reutermarkus1@gmail.com>
2024-01-23 14:00:36 +0100

    Added prints to debug llm prompting

--- semantic_aug/few_shot_dataset.py
+++ semantic_aug/few_shot_dataset.py
@@ -131,7 +131,7 @@ class FewShotDataset(Dataset):
                 prompt_idx = class_occur[class_name] % len(prompts_dict[class_name])
                 self.generative_aug.set_augs_prompt(prompts_dict[class_name][prompt_idx])
 
-            print(f"prompt of TextualInv set to: {self.generative_aug.prompt}")
+            print(f"prompt of TextualInv set to: {self.generative_aug.augs[0].prompt}")
 
             image, label = self.generative_aug(
                 image, label, metadata)
