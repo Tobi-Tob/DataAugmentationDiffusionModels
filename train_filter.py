700d98810b961df8c45e449c2a7e0ff533abbfbe
Tobi <tobi.lingenberg@t-online.de>
2024-01-24 17:32:50 +0100

    added temperature scaling to filter

--- train_filter.py
+++ train_filter.py
@@ -244,13 +244,64 @@ def train_filter(examples_per_class,
                 f"No improvement in validation accuracy for {early_stopping_threshold} epochs. Stopping training.")
             break
 
-    filter_model.load_state_dict(best_filter_model)
+    # Safe the logs
+    log_path = f"logs/train_filter_{seed}_{epoch + 1}x{iterations_per_epoch}.csv"
+    pd.DataFrame.from_records(records).to_csv(log_path)
+
+    # Load the best model
+    filter_model.load_state_dict(best_filter_model).cude()
+
+    """
+    Copyright (c) 2017 Geoff Pleiss
+    https://github.com/gpleiss/temperature_scaling
+    Tune the temperature of the model using the validation set.
+    We're going to set it to optimize NLL.
+    """
+    nll_criterion = nn.CrossEntropyLoss().cuda()
+    ece_criterion = _ECELoss().cuda()
+
+    # Collect all the logits and labels for the validation set
+    logits_list = []
+    labels_list = []
+
+    with torch.no_grad():
+        for image, label in val_dataloader:
+            image = image.cuda()
+
+            logits = filter_model(image)
+
+            logits_list.append(logits)
+            labels_list.append(label)
+
+        logits = torch.cat(logits_list).cuda()
+        labels = torch.cat(labels_list).cuda()
+
+    # Calculate NLL and ECE before temperature scaling
+    before_temperature_nll = nll_criterion(logits, labels).item()
+    before_temperature_ece = ece_criterion(logits, labels).item()
+    print('Start temperature: %.3f' % filter_model.temperature.item())
+    print('Before temperature - NLL: %.3f, ECE: %.3f' % (before_temperature_nll, before_temperature_ece))
+
+    # Next: Optimize the temperature w.r.t. NLL
+    temp_optimizer = torch.optim.LBFGS([filter_model.temperature], lr=0.01, max_iter=10)
+
+    def eval():
+        temp_optimizer.zero_grad()
+        loss = nll_criterion(filter_model.temperature_scale(logits), labels)
+        loss.backward()
+        return loss
+
+    temp_optimizer.step(eval)
+
+    # Calculate NLL and ECE after temperature scaling
+    after_temperature_nll = nll_criterion(filter_model.temperature_scale(logits), labels).item()
+    after_temperature_ece = ece_criterion(filter_model.temperature_scale(logits), labels).item()
+    print('Optimal temperature: %.3f' % filter_model.temperature.item())
+    print('After temperature - NLL: %.3f, ECE: %.3f' % (after_temperature_nll, after_temperature_ece))
+
 
     os.makedirs(model_dir, exist_ok=True)
-    log_path = f"logs/train_filter_{seed}_{epoch + 1}x{iterations_per_epoch}.csv"
     model_path = f"{model_dir}/ClassificationFilterModel.pth"
-
-    pd.DataFrame.from_records(records).to_csv(log_path)
     torch.save(filter_model, model_path)
 
     print(f"Model saved to {model_path} - Validation loss {best_validation_loss} - Validation accuracy "
@@ -263,6 +314,8 @@ class ClassificationFilterModel(nn.Module):
         super(ClassificationFilterModel, self).__init__()
 
         self.image_processor = None
+        self.temperature = nn.Parameter(torch.ones(1) * 1)
+
         self.base_model = resnet50(weights=ResNet50_Weights.DEFAULT)
         self.out = nn.Linear(2048, num_classes)
 
@@ -283,6 +336,59 @@ class ClassificationFilterModel(nn.Module):
             x = torch.flatten(x, 1)
 
         return self.out(x)
+
+    def temperature_scale(self, logits):
+        """
+        Perform temperature scaling on logits
+        """
+        # Expand temperature to match the size of logits
+        temperature = self.temperature.unsqueeze(1).expand(logits.size(0), logits.size(1))
+        return logits / temperature
+
+class _ECELoss(nn.Module):
+    """
+    Calculates the Expected Calibration Error of a model.
+    (This isn't necessary for temperature scaling, just a cool metric).
+
+    The input to this loss is the logits of a model, NOT the softmax scores.
+
+    This divides the confidence outputs into equally-sized interval bins.
+    In each bin, we compute the confidence gap:
+
+    bin_gap = | avg_confidence_in_bin - accuracy_in_bin |
+
+    We then return a weighted average of the gaps, based on the number
+    of samples in each bin
+
+    See: Naeini, Mahdi Pakdaman, Gregory F. Cooper, and Milos Hauskrecht.
+    "Obtaining Well Calibrated Probabilities Using Bayesian Binning." AAAI.
+    2015.
+    """
+    def __init__(self, n_bins=15):
+        """
+        n_bins (int): number of confidence interval bins
+        """
+        super(_ECELoss, self).__init__()
+        bin_boundaries = torch.linspace(0, 1, n_bins + 1)
+        self.bin_lowers = bin_boundaries[:-1]
+        self.bin_uppers = bin_boundaries[1:]
+
+    def forward(self, logits, labels):
+        softmaxes = F.softmax(logits, dim=1)
+        confidences, predictions = torch.max(softmaxes, 1)
+        accuracies = predictions.eq(labels)
+
+        ece = torch.zeros(1, device=logits.device)
+        for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
+            # Calculated |confidence - accuracy| in each bin
+            in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
+            prop_in_bin = in_bin.float().mean()
+            if prop_in_bin.item() > 0:
+                accuracy_in_bin = accuracies[in_bin].float().mean()
+                avg_confidence_in_bin = confidences[in_bin].mean()
+                ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
+
+        return ece
 
 
 if __name__ == "__main__":
