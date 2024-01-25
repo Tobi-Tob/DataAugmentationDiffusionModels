f4796530f8208a6b5ebe100af8f0430d2bc2d6e5
KingEsel1 <reutermarkus1@gmail.com>
2024-01-25 19:56:27 +0100

    enabled command line call for eval_results.py

--- eval_results.py
+++ eval_results.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 from functools import reduce
 
-# Function to process the CSV file
+
 def process_csv(file_path):
     # Read the CSV file
     orig_df = pd.read_csv(file_path)
@@ -18,6 +18,11 @@ def process_csv(file_path):
 
 
 if __name__ == "__main__":
+
+    '''
+    Example call:
+    python --output-dir intermediates/coco_extension/compare_results/ --output-filename comparison.csv --input-paths intermediates/coco_extension/baseline_2/logs/results_0_8.csv intermediates/coco_extension/llm_setting_w_adjective_7/logs/results_0_8.csv
+    '''
 
     parser = argparse.ArgumentParser(description="Simple example of a training script.")
     parser.add_argument(
@@ -41,48 +46,20 @@ if __name__ == "__main__":
     if args.input_paths is None or len(args.input_paths) <= 1:
         raise RuntimeError(f"Found less than 2 files to compare: {args.input_paths}")
 
-    if not args.output_filename.str.endswith('.csv'):
+    if not args.output_filename.endswith('.csv'):
         raise RuntimeError(f"The output filename must be a csv, but was: {args.output_filename}")
 
     best_epochs = []
     for i, file in enumerate(args.input_paths):
-        if not file.str.endswith('.csv'):
+        if not file.endswith('.csv'):
             raise RuntimeError(f"The files must be csv files to be processed, but was: {file}")
 
         best_epochs.append(process_csv(file).rename(columns={'metric': 'metric_', 'value': f'value_file_{i}'}))
 
-    result_df = reduce(lambda df1, df2: pd.merge(df1, df2, on='id'), best_epochs)
+    result_df = reduce(lambda df1, df2: pd.merge(df1, df2, on='metric_', how='outer'), best_epochs)
 
     # Write the merged DataFrame to a CSV file
     if not os.path.exists(args.output_dir):
         os.mkdir(args.output_dir)
     out_path = os.path.join(args.output_dir, args.output_filename)
     result_df.to_csv(out_path, index=False)
-
-
-"""
-# Paths to your CSV files
-file_path_1 = 'intermediates/coco_extension/baseline_2/logs/results_0_8.csv'
-file_path_2 = 'intermediates/coco_extension/llm_setting_w_adjective_7/logs/results_0_8.csv'
-
-# Process both CSV files
-best_epochs_1 = process_csv(file_path_1)
-best_epochs_2 = process_csv(file_path_2)
-print(f"baseline: {best_epochs_1}")
-print(f"llm: {best_epochs_2}")
-
-# Rename 'value' column for clarity
-df1 = best_epochs_1.rename(columns={'metric': 'metric_', 'value': 'value_1'})
-df2 = best_epochs_2.rename(columns={'metric': 'metric_', 'value': 'value_2'})
-
-# Merge the two DataFrames on 'metric'
-merged_df = pd.merge(df1, df2, on='metric_', how='outer')
-
-# Write the merged DataFrame to a CSV file
-file_name = 'comparison.csv'
-out_dir = 'intermediates/coco_extension/compare_results/'
-if not os.path.exists(out_dir):
-    os.mkdir(out_dir)
-out_path = os.path.join(out_dir, file_name)
-merged_df.to_csv(out_path, index=False)
-"""
