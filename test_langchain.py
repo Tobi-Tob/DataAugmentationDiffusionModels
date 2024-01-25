421918321c0a271db1525da4e7911551b5b193c8
KingEsel1 <reutermarkus1@gmail.com>
2024-01-25 20:52:44 +0100

    bugfix test_langchain.py

--- test_langchain.py
+++ test_langchain.py
@@ -3,12 +3,12 @@ import sys
 
 import openai
 from langchain.chains import ConversationalRetrievalChain, RetrievalQA
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import DirectoryLoader, TextLoader
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
-from langchain.llms import OpenAI
+from langchain_community.llms import OpenAI
 from langchain.vectorstores import Chroma
 
 import constants
@@ -29,14 +29,14 @@ if PERSIST and os.path.exists("persist"):
 else:
   #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
   loader = DirectoryLoader("data/")
-  if PERSIST:
-    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
-  else:
-    index = VectorstoreIndexCreator().from_loaders([loader])
+  #MRif PERSIST:
+  #MR  index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
+  #MRelse:
+  #MR  index = VectorstoreIndexCreator().from_loaders([loader])
 
 chain = ConversationalRetrievalChain.from_llm(
   llm=ChatOpenAI(model="gpt-3.5-turbo"),
-  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+  #MR retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
 )
 
 query = "Generate {num_prompts} words to the following question. Only respond with those {num_prompts} words!\n" \
