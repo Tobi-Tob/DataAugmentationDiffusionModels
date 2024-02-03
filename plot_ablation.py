ae4a6649dbd961e99480b001456c48af0a16093e
KingEsel1 <reutermarkus1@gmail.com>
2024-02-03 14:10:43 +0100

    added values for 8epc to plot scripts

--- plot_ablation.py
+++ plot_ablation.py
@@ -5,25 +5,30 @@ import numpy as np
 method = ['Embedding Noise', 'LLM Prompts', 'Filter']
 
 # Data for each set of bars, assuming some example values
-raw_coco_ext_hist = [0.853, 0.859, 0.870]  # Example values for the red bars
-raw_rs_hist = [0.429, 0.464, 0.399]   # Example values for the blue bars
+raw_coco_ext = np.array([0.853, 0.859, 0.870])  # Example values for the red bars
+raw_rs = np.array([0.429, 0.464, 0.399])   # Example values for the blue bars
 raw_baseline_rs = 0.858
 ra_baseline_rs = 0.405
 
 # 4epc
-raw_coco_ext_hist = [0.891, 0.864, 0.902]  # Example values for the red bars
-raw_rs_hist = [0.571, 0.601, 0.542]   # Example values for the blue bars
-raw_baseline_rs = 0.918
-ra_baseline_rs = 0.554
+raw_coco_ext = np.array([0.891, 0.864, 0.902])  # Example values for the red bars
+raw_rs = np.array([0.571, 0.601, 0.542])   # Example values for the blue bars
+raw_baseline_coco = 0.918
+raw_baseline_rs = 0.554
 
 # 8epc
-raw_coco_ext_hist = [0.902, 0.902, ?]  # Example values for the red bars
-raw_rs_hist = [0.684, 0.642, ?]   # Example values for the blue bars
-raw_baseline_rs = ?
-ra_baseline_rs = ?
+raw_coco_ext = np.array([0.902, 0.902, 0.908])  # Example values for the red bars
+raw_rs = np.array([0.684, 0.642, 0.667])   # Example values for the blue bars
+raw_baseline_coco = 0.908
+raw_baseline_rs = 0.649
 
-list_coco = [-0.027, -0.054, -0.016]
-list_rs = [0.017, 0.047, -0.012]
+hist_coco = raw_coco_ext - raw_baseline_coco
+hist_rs = raw_rs - raw_baseline_rs
+
+# TODO:
+# % an y-Achse
+# feste Achsen (-6 bis 6)
+# X-examples per class dranschreiben
 
 # The x position of bars
 x = np.arange(len(method))
@@ -33,8 +38,8 @@ bar_width = 0.35
 
 # Plotting the bars
 fig, ax = plt.subplots()
-bars1 = ax.bar(x - bar_width/2, list_coco, bar_width, label='COCO Extension', color='orange')
-bars2 = ax.bar(x + bar_width/2, list_rs, bar_width, label='Road Sign', color='blue')
+bars1 = ax.bar(x - bar_width/2, hist_coco, bar_width, label='COCO Extension', color='orange')
+bars2 = ax.bar(x + bar_width/2, hist_rs, bar_width, label='Road Sign', color='blue')
 
 # Adding labels and title
 plt.grid(True, axis="y", which="both", ls=":", linewidth=2)
--- plot_lineplot.py
+++ plot_lineplot.py
@@ -11,24 +11,34 @@ examples_per_class = np.array([2, 4, 8])
 plt.figure(figsize=(8, 6))
 
 # COCO General Plot
-# standard_aug = np.array([0.842, 0.869, 0.907])
-# paper = np.array([0.837, 0.897, 0.908])
-# baseline = np.array([0.858, 0.918, 0.918])
-# all_3 = np.array([0.859, 0.880, 0.913])
-# plt.title('COCO Extension', fontsize=22)
+standard_aug = np.array([0.842, 0.869, 0.907])
+paper = np.array([0.837, 0.897, 0.908])
+baseline = np.array([0.858, 0.918, 0.908])
+all_3 = np.array([0.859, 0.880, 0.913])
+llm = np.array([0.859, 0.864, 0.902])
+noise = np.array([0.853, 0.891, 0.902])
+filter = np.array([0.869, 0.902, 0.908])
+best_values = np.array([0.869, 0.902, 0.908])
+plt.title('COCO Extension', fontsize=22)
 
+"""
 # RS General Plot
 standard_aug = np.array([0.440, 0.518, 0.625])
 paper = np.array([0.417, 0.530, 0.667])
-baseline = np.array([0.405, 0.553, 0.553])
+baseline = np.array([0.405, 0.553, 0.649])
 all_3 = np.array([0.405, 0.518, 0.518])
+llm = np.array([0.464, 0.601, 0.642])
+noise = np.array([0.429, 0.571, 0.684])
+filter = np.array([0.399, 0.542, 0.667])
+best_values = np.array([0.464, 0.601, 0.684])
 plt.title('Road Sign', fontsize=22)
+"""
 
 # COCO & RS General Plot
 plt.plot(examples_per_class, standard_aug, label='Standard Augmentation', color='blue', linewidth=4)
 plt.plot(examples_per_class, paper, label='DA-Fusion (Paper params)', color='orange', linewidth=4)
 plt.plot(examples_per_class, baseline, label='DA-Fusion (Our params)', color='green', linewidth=4)
-plt.plot(examples_per_class, all_3, label='Ours Combined', color='red', linewidth=4)
+plt.plot(examples_per_class, best_values, label='Our Best (LLM + Noise)', color='red', linewidth=4)
 
 # Adding labels
 plt.xlabel('Examples Per Class (Size of Dataset)', fontsize=18)
@@ -45,7 +55,7 @@ plt.legend(fontsize=18)
 plt.tight_layout()
 
 # Save the plot as a file
-plt.savefig('plots/test_lineplot_rs_v3.pdf', format='pdf')
+plt.savefig('plots/test_lineplot_coco_best_values.pdf', format='pdf')
 
 # Show the plot
 plt.show()
