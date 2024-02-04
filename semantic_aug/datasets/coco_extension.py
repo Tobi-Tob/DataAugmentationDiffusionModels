dbba9dc2b2cf761ac2ade53d9a16412c96e7a92a
KingEsel1 <reutermarkus1@gmail.com>
2024-02-04 17:08:12 +0100

    hardcoded image for generate augmentations

--- semantic_aug/datasets/coco_extension.py
+++ semantic_aug/datasets/coco_extension.py
@@ -188,7 +188,7 @@ class COCOExtension(FewShotDataset):
         return len(self.all_images)
 
     def get_image_by_idx(self, idx: int) -> torch.Tensor:
-        return Image.open("/data/vilab05/CustomDatasets/Common_Objects/train-val/car/photo_5258008795123144982_y.jpg").convert('RGB')
+        return Image.open("/data/vilab05/CustomDatasets/Common_Objects/train-val/bench/photo_5267111192328001845_y.jpg").convert('RGB')
         # return Image.open(self.all_images[idx]).convert('RGB')
 
     def get_label_by_idx(self, idx: int) -> torch.Tensor:
