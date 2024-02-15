removed normalisation in diverse_test_split.py and fixed copy bug

diverse_test_split.py
@@ -11,7 +11,7 @@ import os
 import warnings
 
 """
-This script performs diverse image selection based on similarity calculations in a ResNet50 feature space.
+This script performs diverse image selection based on cosine similarity calculations in a EfficientNet feature space.
 
 Steps:
 1. Generate Feature Vectors: Feature vectors are extracted for each image in the given directory and saved as a dictionary.
@@ -61,8 +61,8 @@ class CustomDataset(Dataset):
 
 if __name__ == "__main__":
 
-    # image_dir = r'D:/Uni/DLCV/CustomDatasets/Common_Objects/train-val/bench'
-    image_dir = r'D:\Studium\TUDarmstadt\WiSe23_24\DLCV\datasets\merged_images_custom_dataset\train-val\test_algo2'
+    image_dir = r'D:\Uni\DLCV\CustomDatasets\Common_Objects\train-val\test'
+    # image_dir = r'D:\Studium\TUDarmstadt\WiSe23_24\DLCV\datasets\merged_images_custom_dataset\train-val\test_algo2'
     n = 5  # Number of images to find, including the starting image
 
     # Initialize the dataset and dataloader
@@ -89,23 +89,6 @@ if __name__ == "__main__":
                 output = feature_extractor(image).squeeze()
                 feature_vectors[path] = output.numpy()
 
-    # Evaluating the feature space
-    all_feature_vectors = np.array(list(feature_vectors.values()))
-    # Calculate the mean and standard deviation across the 0th axis (i.e., for each feature dimension)
-    mean_vector = np.mean(all_feature_vectors, axis=0)
-    std_dev_vector = np.std(all_feature_vectors, axis=0)
-    print("Mean of feature vectors:", mean_vector)
-    print("Standard deviation of feature vectors:", std_dev_vector)
-    print("feature vectors", feature_vectors.values())
-
-    # Normalize each feature vector
-    normalized_feature_vectors = {}
-    for path, vector in feature_vectors.items():
-        normalized_vector = (vector - mean_vector) / std_dev_vector
-        normalized_feature_vectors[path] = normalized_vector
-    print("normalized feature vectors", normalized_feature_vectors.values())
-    feature_vectors = normalized_feature_vectors
-
     num_feature_vectors = len(feature_vectors)
     if num_feature_vectors > 0:
         # Get the size of the feature vector for the first image
@@ -119,19 +102,15 @@ if __name__ == "__main__":
 
     # Extract the vectors and calculate the distance matrix
     vectors = np.array(list(feature_vectors.values()))
-    # distance_matrix = cdist(vectors, vectors, 'euclidean')  # TODO: Try different methods
-
+    # distance_matrix = cdist(vectors, vectors, 'euclidean')
     distance_matrix = cdist(vectors, vectors, 'cosine')
-    # -Convert cosine distances to cosine similarities
-    cosine_similarity_matrix = 1 - distance_matrix
-    distance_matrix = cosine_similarity_matrix
 
     # Extract the image with the smallest distance to all other images as starting point
     distance_sums = distance_matrix.sum(axis=1)
     starting_index = np.argmin(distance_sums)
     starting_image_path = list(feature_vectors.keys())[starting_index]
     testset_images = [os.path.basename(starting_image_path)]
-    temp_max_distances = distance_matrix[starting_index]
+    temp_max_distances = distance_matrix[starting_index].copy()
     selected_indices = [starting_index]
 
     print("Selecting images in feature space...")